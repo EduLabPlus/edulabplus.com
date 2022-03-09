5798f6990b6b6213968ce349ffa9c8fc99cd909a
bash243 <abash2002@gmail.com>
2022-03-09 11:03:59 +0400

    Gallery + enhancements
    Jawa img, icons, gallery image updates, menu updates, js file added, css modified

--- js/stats_f.js
+++ js/stats_f.js
@@ -1,26 +1,32 @@
-    // number count for stats, using jQuery animate
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter-stats').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
     $('.counting').each(function() {
       var $this = $(this),
-          countTo = $this.attr('data-count');
-      
-      $({ countNum: $this.text()}).animate({
-        countNum: countTo
-      },
-    
-      {
-    
-        duration: 3000,
-        easing:'linear',
-        step: function() {
-          $this.text(Math.floor(this.countNum));
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
         },
-        complete: function() {
-          $this.text(this.countNum + '+');
-          //alert('finished');
-        }
-    
-      });  
-      
-    });
 
-  
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
