3164234da5c4ffd1e7081758a82b905665da3453
Shahul Hameed Badhusha <shahul@makinus.com>
2022-03-11 21:59:58 +0530

    Updated on download certificate

--- js/certificate.js
+++ js/certificate.js
@@ -2,6 +2,6 @@ function downloadCertificate() {
     var formData = $('form.certificateForm').serialize();	
         $.ajax({
             type: "POST",                
-            url: 'https://htmlservice.herokuapp.com/api',				
+            url: 'https://htmlservice.herokuapp.com/api/edulabplus/expo/certificate',				
             data: formData});
             }
