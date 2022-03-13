180244e2e5d2814b2e0392335bb5ad975da91b44
Abubacker Siddik A <abu@makinus.com>
2022-03-13 22:44:49 +0530

    Add expo certificate options

--- js/certificate.js
+++ js/certificate.js
@@ -1,7 +1,27 @@
-function downloadCertificate() {
-    var formData = $('form.certificateForm').serialize();	
+jQuery(document).ready(function($) {
+"use strict";
+
+    function downloadExpoCertificate() {
+        var formData = $('form.certificateForm').serialize();
         $.ajax({
-            type: "POST",                
+            type: "GET",                
             url: 'https://htmlservice.herokuapp.com/api/edulabplus/expo/certificate',				
-            data: formData});
+            data: formData,
+            beforeSend: function(){
+                $('#loading').addClass("d-block").removeClass("d-none");
+             },
+            success: function(data) {
+               $("#sendmessage").addClass("d-block");
+               $("#errormessage").removeClass("d-block");
+               $('#loading').removeClass("d-block").addClass("d-none");
+            },
+            error: function() {
+               $("#sendmessage").removeClass("d-block");
+               $("#errormessage").addClass("d-block");
+               $('#errormessage').html("Error occurred. Please try again");
+               $('#loading').removeClass("d-block").addClass("d-none");
             }
+        });
+    }
+
+});
