cd7b7ebedc5c966988b8f790bfe396004b715b43
Abubacker Siddik A <abu@makinus.com>
2022-03-13 23:26:41 +0530

    Update expo certificate js

--- js/certificate.js
+++ js/certificate.js
@@ -7,14 +7,50 @@ jQuery(document).ready(function($) {
             type: "GET",                
             url: 'https://htmlservice.herokuapp.com/api/edulabplus/expo/certificate',				
             data: formData,
-            responseType: "blob",
+            xhrFields: {
+                responseType: 'blob' // to avoid binary data being mangled on charset conversion
+            },
             beforeSend: function(){
                 $('#loading').addClass("d-block").removeClass("d-none");
-             },
-            success: function(data) {
-               $("#sendmessage").addClass("d-block");
-               $("#errormessage").removeClass("d-block");
-               $('#loading').removeClass("d-block").addClass("d-none");
+            },
+            success: function(blob, status, xhr) {
+                // check for a filename
+                var filename = "";
+                var disposition = xhr.getResponseHeader('Content-Disposition');
+                if (disposition && disposition.indexOf('attachment') !== -1) {
+                    var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                    var matches = filenameRegex.exec(disposition);
+                    if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+                }
+        
+                if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                    // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+                    window.navigator.msSaveBlob(blob, filename);
+                } else {
+                    var URL = window.URL || window.webkitURL;
+                    var downloadUrl = URL.createObjectURL(blob);
+        
+                    if (filename) {
+                        // use HTML5 a[download] attribute to specify filename
+                        var a = document.createElement("a");
+                        // safari doesn't support this yet
+                        if (typeof a.download === 'undefined') {
+                            window.location.href = downloadUrl;
+                        } else {
+                            a.href = downloadUrl;
+                            a.download = filename;
+                            document.body.appendChild(a);
+                            a.click();
+                        }
+                    } else {
+                        window.location.href = downloadUrl;
+                    }
+        
+                    setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+                }
+                $("#sendmessage").addClass("d-block");
+                $("#errormessage").removeClass("d-block");
+                $('#loading').removeClass("d-block").addClass("d-none");
             },
             error: function() {
                $("#sendmessage").removeClass("d-block");
